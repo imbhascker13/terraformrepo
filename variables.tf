924a1b930dd93786b0baf590422de6eedba5719f
imbhascker13 <58689890+imbhascker13@users.noreply.github.com>
2020-02-18 11:15:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "key_name" {
-  default = "meghanamumbai"
+  default = "josis"
 }
 
 variable "pvt_key" {
-  default = "/root/.ssh/mkey.pem"
+  default = "/root/.ssh/josis.pem"
 }
 
 variable "us-east-zones" {
-  default = ["us-east-1a", "us-east-1b"]
+  default = ["us-west-1a", "us-west-1b"]
 }
 
 variable "sg-id" {
-  default = "sg-0eb024eb164eb86c0"
+  default = "sg-0172a1313f34a8ba8"
 }
